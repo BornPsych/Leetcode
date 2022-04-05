31f7f3dae2213047be4fcbc8cc77287c21121b87
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2022-04-05 19:05:52 +0530

    Time: 7 ms (57.96%), Space: 7.2 MB (32.85%) - LeetHub

---
+++ 415-add-strings/415-add-strings.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        vector<int>v;
+        int c=0;
+        string s1="";
+        while(true){
+            if(num1=="" && num2=="")break;
+            int x=0,y=0;
+            if(num1.length()){
+                x = num1.back()-'0';
+                num1.pop_back();
+            }
+            
+            if(num2.length()){
+                y = num2.back()-'0';
+                num2.pop_back();
+            }
+            
+            int sum=c+ x+y;
+            v.push_back(sum%10);
+            c = sum/10;
+            
+        }
+        if(c)v.push_back(c);
+        
+        reverse(v.begin(),v.end());
+        
+        for(auto i:v){
+            s1+=(i+'0');
+        }
+        
+        return s1;
+    }
+};
