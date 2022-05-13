1c395ac7a39778b90eba73c5e45da6f875f8e48a
Yogesh Shahi <65803503+BornPsych@users.noreply.github.com>
2022-05-13 19:14:44 +0530

    Time: 122 ms (47.73%), Space: 10.6 MB (79.99%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -8,10 +8,6 @@ public:
                 if(i==0 && j==0)dp[i][j]=INT_MAX-1;
                 else if(i==0)dp[i][j]=INT_MAX-1;
                 else if(j==0)dp[i][j]=0;
-                else if(i==1){
-                    if(j%coins[i-1])dp[i][j]=INT_MAX-1;
-                    else dp[i][j]=j/coins[i-1];
-                }
                 else if(j>=coins[i-1]){
                     dp[i][j] = min(dp[i-1][j],dp[i][j-coins[i-1]]+1);
                 }else{
